95d0cedda4d77d596b4bf390d9165bb7faa4a11b
thanhtruc100diem <trucdang28052005@gmail.com>
2024-09-15 11:42:02 +0700

    Add files via upload

---
+++ viduminhhoa7.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 15 11:18:12 2024
+
+@author: Student
+"""
+
+x=float(input("nhap x="))
+while x<0:
+    x=float(input("nhap lai x="))
+else:
+    print("gia tri da nhap:",x)
